8b032e4f6c337b6a93391c40ccadd1ff8b68d4cb
jakeuj <jakeuj@hotmail.com>
2020-05-13 05:11:30 +0800

    Init

---
+++ MyKirito/Program.cs
@@ -0,0 +1,408 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace MyKirito
+{
+    internal class Program
+    {
+        // 行動列舉值
+        public enum ActionEnum
+        {
+            Sit,
+            Good,
+            Train,
+            Eat,
+            Girl,
+            Hunt
+        }
+
+        // 遊戲檢查時間闕值 (預設:90秒)
+        private const int CheckTime = 90000;
+
+        // 遊戲Pvp檢查時間闕值 (預設:402秒)
+        private const int PvpTime = 402;
+
+        // TODO:需移除
+        // 開發階段預設Token
+        private static string _token = string.Empty;
+
+        private static int _totalPoints;
+
+        // Pvp計時器
+        private static DateTime _nextPvpTime = DateTime.Now.AddSeconds(PvpTime);
+
+        // 預設動作
+        private static ActionEnum _defaultAct = ActionEnum.Hunt;
+
+        // 額外屬性等級闕值
+        private static readonly int[] AddPointLevel = {10, 16, 20, 23, 25, 27, 29, 31};
+
+        // 程式進入點
+        private static async Task<int> Main(string[] args)
+        {
+            // Console.OutputEncoding = Encoding.UTF8;
+            // 初始化
+            Init();
+            using var host = Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging
+                        .AddFilter("Microsoft", LogLevel.Warning)
+                        .AddFilter("System", LogLevel.Warning)
+                        .AddFilter("LoggingConsoleApp.Program", LogLevel.Debug)
+                        .AddConsole();
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    // 遊戲API端點
+                    services.AddHttpClient("kiritoAPI", c =>
+                    {
+                        c.BaseAddress = new Uri("https://mykirito.com/api/");
+                        c.DefaultRequestHeaders.Add("token", _token);
+                    });
+                    // 遊戲公開資訊端點
+                    services.AddHttpClient("kiritoInfo", c =>
+                    {
+                        c.BaseAddress = new Uri("https://us-central1-kirito-1585904519813.cloudfunctions.net/");
+                        c.DefaultRequestHeaders.Add("token", _token);
+                    });
+                    // 註冊遊戲服務
+                    services.AddTransient<IMyService, MyService>();
+                    // 註冊排程服務
+                    services.AddHostedService<ConsumeScopedServiceHostedService>();
+                    services.AddScoped<IScopedProcessingService, ScopedProcessingService>();
+                })
+                .Build();
+            // 開始排程服務
+            await host.StartAsync();
+
+            // 等待服務關閉
+            await host.WaitForShutdownAsync();
+
+            return 0;
+        }
+
+        private static void Init()
+        {
+            // 更新授權Token
+            Console.WriteLine("[Required] Input your token:");
+            string newInput;
+            while (true)
+            {
+                newInput = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(newInput))
+                {
+                    _token = newInput;
+                    Console.WriteLine($"Token is update to: {newInput}");
+                    break;
+                }
+
+                Console.WriteLine("[Required] Input your token, know?");
+            }
+
+            // 更新預設動作
+            Console.Write("[Optional] Input your action from:");
+            foreach (var name in Enum.GetNames(typeof(ActionEnum))) Console.Write($" {name} ");
+            Console.WriteLine();
+            newInput = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(newInput))
+            {
+                _defaultAct = (ActionEnum) Enum.Parse(typeof(ActionEnum), newInput);
+                Console.WriteLine($"Action is update to: {_defaultAct.ToString()}");
+            }
+        }
+
+        //檢查獲得屬性點數
+        private static int CheckPoint(int input)
+        {
+            //超過最大等級時：超過幾級就拿幾點 + 所有獎勵等級各拿一點(有幾個限制就拿幾點)
+            if (input > AddPointLevel.Max())
+                return input - AddPointLevel.Max() + AddPointLevel.Count();
+            //否則等級滿足幾個門檻就拿點幾額外屬性點
+            return AddPointLevel.Count(x => input >= x);
+        }
+
+        // 角色資料傳輸物件
+        public class MyKiritoDto
+        {
+            //TODO: 顯示中文
+            public string NickName { get; set; }
+            public int Lv { get; set; }
+            public int Exp { get; set; }
+            public bool Dead { get; set; }
+        }
+
+        // 遊戲服務介面
+        public interface IMyService
+        {
+            // 遊戲主畫面操作
+            Task<string> DoAction(ActionEnum input);
+
+            // 遊戲投胎機制
+            Task<string> ReIncarnation(int freePoints);
+
+            // 取得遊戲角色資料
+            Task<MyKiritoDto> GetMyKiritoFn();
+            Task<string> GetUserList(int exp);
+        }
+
+        // 遊戲服務介面實作類別
+        public class MyService : IMyService
+        {
+            // 發起Http要求
+            private readonly IHttpClientFactory _clientFactory;
+
+            // DI注入
+            public MyService(IHttpClientFactory clientFactory)
+            {
+                _clientFactory = clientFactory;
+            }
+
+            /// <summary>
+            ///     角色轉生
+            /// </summary>
+            /// <param name="freePoints">升級點數</param>
+            /// <returns>結果</returns>
+            public async Task<string> ReIncarnation(int freePoints)
+            {
+                Console.WriteLine($"獲得 {freePoints} 屬性");
+                var request = new HttpRequestMessage(HttpMethod.Post, "my-kirito/reincarnation");
+                // 將點數點到生命值
+                var restLoad =
+                    $"{{\"character\":\"kirito\",\"rattrs\":{{\"hp\":{freePoints},\"atk\":0,\"def\":0,\"stm\":0,\"agi\":0,\"spd\":0,\"tec\":0,\"int\":0,\"lck\":0}},\"useReset\":false}}";
+                request.Content = new StringContent(restLoad, Encoding.UTF8, "application/json");
+                var client = _clientFactory.CreateClient("kiritoAPI");
+                // 送出轉生請求
+                var response = await client.SendAsync(request);
+                // 結果
+                if (!response.IsSuccessStatusCode) return $"StatusCode: {response.StatusCode}";
+                // 總計下屬性點
+                _totalPoints += freePoints;
+                return await response.Content.ReadAsStringAsync();
+            }
+
+            /// <summary>
+            ///     角色行動
+            /// </summary>
+            /// <param name="input">要汁妹還是做啥來著</param>
+            /// <returns>結果</returns>
+            public async Task<string> DoAction(ActionEnum input)
+            {
+                var request = new HttpRequestMessage(HttpMethod.Post, "my-kirito/doaction")
+                {
+                    Content = new StringContent($"{{\"action\":\"{input.ToString().ToLower()}\"}}", Encoding.UTF8,
+                        "application/json")
+                };
+                var client = _clientFactory.CreateClient("kiritoAPI");
+                // 送出行動請求
+                var response = await client.SendAsync(request);
+                // 結果
+                if (response.IsSuccessStatusCode)
+                    return await response.Content.ReadAsStringAsync();
+                return $"StatusCode: {response.StatusCode}";
+            }
+
+            /// <summary>
+            ///     取得角色資料
+            /// </summary>
+            /// <returns>角色物件JSON</returns>
+            public async Task<MyKiritoDto> GetMyKiritoFn()
+            {
+                var request = new HttpRequestMessage(HttpMethod.Get, "getMyKiritoFn");
+                var client = _clientFactory.CreateClient("kiritoInfo");
+                var response = await client.SendAsync(request);
+                // 不成功則空
+                if (!response.IsSuccessStatusCode) return new MyKiritoDto();
+                // 成功則反序列化後返回角色物件
+                var res = await response.Content.ReadAsStringAsync();
+                return JsonSerializer.Deserialize<MyKiritoDto>(res,
+                    new JsonSerializerOptions {PropertyNameCaseInsensitive = true});
+            }
+
+            // 查詢PvP玩家列表
+            public async Task<string> GetUserList(int exp)
+            {
+                var request = new HttpRequestMessage(HttpMethod.Get, $"user-list?exp={exp}");
+                var client = _clientFactory.CreateClient("kiritoAPI");
+                // 送出行動請求
+                var response = await client.SendAsync(request);
+                // 結果
+                if (response.IsSuccessStatusCode)
+                {
+                    var res = await response.Content.ReadAsStringAsync();
+
+                    using (var document = JsonDocument.Parse(res))
+                    {
+                        var root = document.RootElement;
+                        var studentsElement = root.GetProperty("userList");
+                        foreach (var student in studentsElement.EnumerateArray())
+                        {
+                            if (!student.TryGetProperty("uid", out var gradeElement) ||
+                                !student.TryGetProperty("lv", out var lvElement) ||
+                                !student.TryGetProperty("color", out var colorElement) ||
+                                colorElement.GetString() == "grey") continue;
+
+                            var uid = gradeElement.GetString();
+
+                            var lv = lvElement.GetInt32();
+
+                            var (isSuccess, msg) = await Challenge(uid, lv);
+
+                            if (isSuccess)
+                            {
+                                _nextPvpTime = _nextPvpTime.AddSeconds(PvpTime);
+                                return msg;
+                            }
+                        }
+                    }
+
+                    return await response.Content.ReadAsStringAsync();
+                }
+
+                return $"StatusCode: {response.StatusCode}";
+            }
+
+            // 出戰Pvp
+            private async Task<(bool isSuccess, string msg)> Challenge(string uid, int lv)
+            {
+                var request = new HttpRequestMessage(HttpMethod.Post, "challenge")
+                {
+                    Content = new StringContent($"{{\"type\":1,\"opponentUID\":\"{uid}\",\"shout\":\"\",\"lv\":{lv}}}",
+                        Encoding.UTF8,
+                        "application/json")
+                };
+                var client = _clientFactory.CreateClient("kiritoAPI");
+                // 送出行動請求
+                var response = await client.SendAsync(request);
+                // 結果
+                return response.IsSuccessStatusCode
+                    ? (true, await response.Content.ReadAsStringAsync())
+                    : (false, $"StatusCode: {response.StatusCode}");
+            }
+        }
+
+        // 背景工作排程服務
+        internal interface IScopedProcessingService
+        {
+            Task DoWork(CancellationToken stoppingToken);
+        }
+
+        internal class ScopedProcessingService : IScopedProcessingService
+        {
+            private readonly ILogger _logger;
+
+            // 排程主要使用遊戲主服務
+            private readonly IMyService _myService;
+            private int _executionCount;
+
+            public ScopedProcessingService(ILogger<ScopedProcessingService> logger, IMyService myService)
+            {
+                _logger = logger;
+                _myService = myService;
+            }
+
+            // 執行工作
+            public async Task DoWork(CancellationToken stoppingToken)
+            {
+                while (!stoppingToken.IsCancellationRequested)
+                {
+                    _executionCount++;
+
+                    _logger.LogInformation(
+                        "Scoped Processing Service is working. Count: {Count}", _executionCount);
+                    // 取得角色資料
+                    var myKirito = await _myService.GetMyKiritoFn();
+                    // Log角色資料
+                    Console.WriteLine(JsonSerializer.Serialize(myKirito,
+                        new JsonSerializerOptions {WriteIndented = true}));
+                    // 轉生限制條件：滿十等或死亡
+                    if (myKirito.Dead || myKirito.Lv >= 10)
+                    {
+                        // 計算轉生屬性點數
+                        var freePoints = CheckPoint(myKirito.Lv);
+                        // 開始轉生
+                        var result = await _myService.ReIncarnation(freePoints);
+                        // Log轉生結果
+                        Console.WriteLine(result);
+                    }
+                    else
+                    {
+                        // 日常動作：汁妹之類的
+                        var result = await _myService.DoAction(_defaultAct);
+                        // Log動作結果
+                        Console.WriteLine(result);
+                        // PVP 
+                        if (DateTime.Now > _nextPvpTime)
+                        {
+                            result = await _myService.GetUserList(myKirito.Exp);
+                            Console.WriteLine(result);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Next PVP time is {_nextPvpTime.ToShortTimeString()}");
+                        }
+                    }
+
+                    Console.WriteLine($"獲得屬性小計：{_totalPoints}");
+                    // 定時執行
+                    await Task.Delay(CheckTime, stoppingToken);
+                }
+            }
+        }
+
+        // 背景服務
+        public class ConsumeScopedServiceHostedService : BackgroundService
+        {
+            private readonly ILogger<ConsumeScopedServiceHostedService> _logger;
+
+            public ConsumeScopedServiceHostedService(IServiceProvider services,
+                ILogger<ConsumeScopedServiceHostedService> logger)
+            {
+                Services = services;
+                _logger = logger;
+            }
+
+            public IServiceProvider Services { get; }
+
+            protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+            {
+                _logger.LogInformation(
+                    "Consume Scoped Service Hosted Service running.");
+
+                await DoWork(stoppingToken);
+            }
+
+            private async Task DoWork(CancellationToken stoppingToken)
+            {
+                _logger.LogInformation(
+                    "Consume Scoped Service Hosted Service is working.");
+
+                using (var scope = Services.CreateScope())
+                {
+                    var scopedProcessingService =
+                        scope.ServiceProvider
+                            .GetRequiredService<IScopedProcessingService>();
+
+                    await scopedProcessingService.DoWork(stoppingToken);
+                }
+            }
+
+            public override async Task StopAsync(CancellationToken stoppingToken)
+            {
+                _logger.LogInformation(
+                    "Consume Scoped Service Hosted Service is stopping.");
+
+                await Task.CompletedTask;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,183 @@
-# MyKirito
-MyKirito
+# 動畫推薦清單
+![https://www.pixiv.net/artworks/78875342](78875342_p0.jpg)
+
+[圖源：Pixiv-78875342](https://www.pixiv.net/artworks/78875342)
+
+有想協作推薦的可以Fork後發PR補充作品！
+
+## 奇幻冒險
+
+- [NO GAME NO LIFE 遊戲人生](https://ani.gamer.com.tw/animeVideo.php?sn=7944)
+- [Re：從零開始的異世界生活](https://ani.gamer.com.tw/animeVideo.php?sn=4996)
+- [灰與幻想的格林姆迦爾](https://ani.gamer.com.tw/animeVideo.php?sn=11689)
+- [刀劍神域](https://ani.gamer.com.tw/animeVideo.php?sn=926)
+- [加速世界](https://www.58b.tv/vod-read-id-45417.html)
+- [OVERLORD](https://ani.gamer.com.tw/animeVideo.php?sn=12435)
+- [大劍](https://www.58b.tv/vod-read-id-6316.html)
+- [反叛的魯路修](https://www.58b.tv/vod-read-id-7470.html)
+- [在地下城尋求邂逅是否搞錯了什麼](https://ani.gamer.com.tw/animeVideo.php?sn=7624)
+- [灼眼的夏娜](https://www.58b.tv/vod-read-id-24441.html)
+- [薔薇少女](https://www.58b.tv/vod-read-id-7707.html)
+- [哥布林殺手](https://ani.gamer.com.tw/animeVideo.php?sn=10852)
+- [關於我轉生變成史萊姆這檔事](https://ani.gamer.com.tw/animeVideo.php?sn=10828)
+- [爆肝工程師的異世界狂想曲](https://ani.gamer.com.tw/animeVideo.php?sn=9296)
+- [異世界魔王與召喚少女的奴隸魔術](https://ani.gamer.com.tw/animeVideo.php?sn=11977)
+- [線上遊戲的老婆不可能是女生？](https://ani.gamer.com.tw/animeVideo.php?sn=5012)
+- [為美好的世界獻上祝福！](https://ani.gamer.com.tw/animeVideo.php?sn=7294)
+- [Dr.STONE 新石紀](https://ani.gamer.com.tw/animeVideo.php?sn=12653)
+- [超人高中生們即便在異世界也能從容生存](https://ani.gamer.com.tw/animeVideo.php?sn=14409)
+- [青春豬頭少年不會夢到兔女郎學姊](https://ani.gamer.com.tw/animeVideo.php?sn=10837)
+- [鬼滅之刃](https://ani.gamer.com.tw/animeVideo.php?sn=12083)
+- [魔法少女小圓](https://ani.gamer.com.tw/animeVideo.php?sn=792)
+- [魔法少女奈葉](https://www.58b.tv/vod-read-id-69333.html)
+  - [x] [魔法少女奈葉(無印)](https://www.58b.tv/vod-read-id-7516.html)
+  - [x] [魔法少女奈葉ViVid](https://www.58b.tv/vod-read-id-73283.html)
+  - [x] [魔法少女奈葉StrikerS](https://www.58b.tv/vod-read-id-86481.html)
+  - [x] [ViVid Strike!](https://ani.gamer.com.tw/animeVideo.php?sn=11268)
+- [幼女戰記](https://ani.gamer.com.tw/animeVideo.php?sn=7148)
+- [絕園的暴風雨](https://ani.gamer.com.tw/animeVideo.php?sn=437)
+- [K](https://ani.gamer.com.tw/animeVideo.php?sn=10649)
+- [魔法禁書目錄](https://ani.gamer.com.tw/animeVideo.php?sn=4557)
+  - [外傳 科學超電磁砲](https://ani.gamer.com.tw/animeVideo.php?sn=4581)
+  - [科學一方通行](https://ani.gamer.com.tw/animeVideo.php?sn=12688)
+- [約會大作戰 DATE A LIVE](https://ani.gamer.com.tw/animeVideo.php?sn=14126)
+- [斬！赤紅之瞳](https://ani.gamer.com.tw/animeVideo.php?sn=7870)
+- [寶石之國](https://ani.gamer.com.tw/animeVideo.php?sn=8786)
+- [機巧少女不會受傷](https://ani.gamer.com.tw/animeVideo.php?sn=8093)
+- [末日時在做什麼？有沒有空？可以來拯救嗎？](https://ani.gamer.com.tw/animeVideo.php?sn=7606)
+- [SSSS.GRIDMAN](https://ani.gamer.com.tw/animeVideo.php?sn=10855)
+- [IS 〈Infinite Stratos〉](https://ani.gamer.com.tw/animeVideo.php?sn=4096)
+- [發條精靈戰記 天鏡的極北之星](https://ani.gamer.com.tw/animeVideo.php?sn=5872)
+- [動物朋友](https://ani.gamer.com.tw/animeVideo.php?sn=7747)
+- [這個勇者明明超 TUEEE 卻過度謹慎！](https://ani.gamer.com.tw/animeVideo.php?sn=13869)
+- [緋彈的亞里亞](https://www.58b.tv/vod-read-id-30059.html)
+- [Sola](https://www.58b.tv/vod-read-id-7427.html)
+- [C3魔方少女](https://www.58b.tv/vod-read-id-41224.html)
+- [H2O赤沙印記](https://www.58b.tv/vod-read-id-55843.html)
+
+## 科幻未來
+
+- [混沌之子](https://ani.gamer.com.tw/animeVideo.php?sn=7156)
+- [命運石之門](https://www.58b.tv/vod-read-id-33343.html)
+- 彼方的阿斯特拉
+- [ALDNOAH.ZERO](https://zh.wikipedia.org/zh-tw/ALDNOAH.ZERO)
+
+## 青春校園
+
+- [歡迎來到實力至上主義的教室](https://ani.gamer.com.tw/animeVideo.php?sn=8542)
+- [K-ON！輕音部](https://ani.gamer.com.tw/animeVideo.php?sn=4080)
+  - [K-ON！！輕音部](https://ani.gamer.com.tw/animeVideo.php?sn=3193)
+- [學生會的一己之見](https://www.58b.tv/vod-read-id-13103.html)
+- [我的朋友很少](https://www.58b.tv/vod-read-id-41319.html)
+- [學園默示錄](https://www.58b.tv/vod-read-id-20835.html)
+- 夏洛特
+
+## 幽默搞笑
+
+- [監獄學園](https://ani.gamer.com.tw/animeVideo.php?sn=7208)
+- [入間同學入魔了](https://ani.gamer.com.tw/animeVideo.php?sn=13809)
+- [遊戲 3 人娘](https://ani.gamer.com.tw/animeVideo.php?sn=10258)
+- [單蠢女孩](https://ani.gamer.com.tw/animeVideo.php?sn=8321)
+- [情色漫畫老師](https://ani.gamer.com.tw/animeVideo.php?sn=11590)
+- [魔術學姐](https://ani.gamer.com.tw/animeVideo.php?sn=12640)
+- [三顆星彩色冒險](https://ani.gamer.com.tw/animeVideo.php?sn=9319)
+- [超能力者齊木楠雄的災難](https://www.58b.tv/vod-read-id-84855.html)
+- [電波少女](https://www.58b.tv/vod-read-id-10604.html)
+- [我的妹妹不可能這麼可愛](https://www.58b.tv/vod-read-id-32815.html)
+- [GAMERS電玩咖](https://www.58b.tv/vod-read-id-93856.html)
+- 後街女孩
+
+## 戀愛
+
+- [果然我的青春戀愛喜劇搞錯了](https://ani.gamer.com.tw/animeVideo.php?sn=3295)
+- [輝夜姬想讓人告白～天才們的戀愛頭腦戰～](https://ani.gamer.com.tw/animeVideo.php?sn=11431)
+- [我的腦內戀礙選項](https://ani.gamer.com.tw/animeVideo.php?sn=9776)
+- [戀愛與謊言](https://ani.gamer.com.tw/animeVideo.php?sn=8316)
+- [家有女友](https://ani.gamer.com.tw/animeVideo.php?sn=11412)
+- [Just Because](https://ani.gamer.com.tw/animeVideo.php?sn=8782)
+- [天降之物](https://www.58b.tv/vod-read-id-13401.html)
+- [只有神知道的世界](https://www.58b.tv/vod-read-id-23765.html)
+- [架向星空的橋](https://www.58b.tv/vod-read-id-32920.html)
+- [尋找失去的未來](https://ani.gamer.com.tw/animeVideo.php?sn=1354)
+- 鄰座的怪同學
+- 徒然喜歡你
+- 笨拙的上野
+
+## 溫馨
+
+- [工作細胞](https://ani.gamer.com.tw/animeVideo.php?sn=10210)
+- [四月是你的謊言](https://www.58b.tv/vod-read-id-68592.html)
+- [未聞花名](https://www.58b.tv/vod-read-id-31527.html)
+- [CLANNAD](https://www.58b.tv/vod-read-id-53920.html)
+- [要聽爸爸的話](https://www.58b.tv/vod-read-id-43814.html)
+- [狼與辛香料](https://www.58b.tv/vod-read-id-4080.html)
+
+## 靈異神怪
+
+- [這個是殭屍嗎](https://www.58b.tv/vod-read-id-28456.html)
+
+## 推理懸疑
+
+- [死亡筆記本](https://ani.gamer.com.tw/animeVideo.php?sn=10579)
+- [只有我不存在的城市](https://ani.gamer.com.tw/animeVideo.php?sn=4877)
+- [重啟咲良田](https://ani.gamer.com.tw/animeVideo.php?sn=7590)
+- [黃昏少女X失憶](https://www.58b.tv/vod-read-id-45571.html)
+- [散華禮彌](https://www.58b.tv/vod-read-id-45492.html)
+- [AnOther](https://www.58b.tv/vod-read-id-43775.html)
+- [GoSick](https://www.58b.tv/vod-read-id-28342.html)
+- [MyselfYourself](https://www.58b.tv/vod-read-id-10773.html)
+- [冰果](https://www.58b.tv/vod-read-id-46038.html)
+- 神的記事本
+
+## 料理美食
+
+## 社會寫實
+
+- 機動戰士鋼彈
+- 鐵血孤兒
+
+## 運動競技
+
+- [龍王的工作！](https://ani.gamer.com.tw/animeVideo.php?sn=9285)
+- 花牌情緣
+
+## 歷史傳記
+
+- 漂流武士
+
+## 其他
+
+- [黃金神威](https://www.58b.tv/vod-read-id-99942.html)
+- 街角魔族
+- 小林家的龍女僕
+- 天使降臨我身邊
+- 柑橘味香氣
+- 終將成為你
+- 進擊的巨人
+- JOJO
+- 聲之形
+- 凪的明日
+- 來自繽紛世界的明天
+- 煙草
+- 魔法戰爭
+- 迷家
+- 國王遊戲
+- 黑之契約者
+- 約定的夢幻島
+- 笑園漫畫大王
+- 刺客伍六七
+- 奇諾之旅
+- 紫羅蘭永恆花園
+- 路人超能100
+- 高分少女
+- 魔法使的新娘
+- 大劍
+- 蟲師
+- 來自新世界
+- 寄生獸
+- 烙印勇士
+- 零之使魔
+- 禁忌咒紋
+- MUSASHI -GUN道-
+- 正確的卡多
+
